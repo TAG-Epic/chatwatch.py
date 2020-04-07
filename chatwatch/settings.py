e5533b94540cb8abecddb393c7b7e80bfe381f3b
Epic <admin@itzepic.net>
2020-04-07 13:44:54 +0200

    Bump version

--- chatwatch/settings.py
+++ chatwatch/settings.py
@@ -3,7 +3,7 @@ Settings for the ChatWatch.py library.
 """
 __version__ = (0, 0, 1)
 # pylint: disable=invalid-name
-version_string = "0.0.1"
+version_string = "0.0.2"
 library_stamp = "chatwatch(https://github.com/TAG-Epic/chatwatch.py, {})".format(version_string)
 base_logger = "chatwatch."
 usage_stat_url = "https://stat.farfrom.world/package/chatwatch.py/register"
